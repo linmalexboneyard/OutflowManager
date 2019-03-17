4c15f1bfb820046c96787010d9e8164f84063a95
Lindsay Alexander <linmalex@gmail.com>
2019-03-17 15:09:00 -0700

    add expense class

---
+++ OutflowManager/Models/Expense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OutflowManager.Models
+{
+    public class Expense
+    {
+        public string Name { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime Date { get; set; }
+        public float PriceLow { get; set; }
+        public float PriceHigh { get; set; }
+        public int PriorityValue { get; set; }
+        public string Category { get; set; }
+        public string ProblemToSolve { get; set; }
+        public string TemporarySolution { get; set; }
+        public string TimeBaseSolution { get; set; }
+    }
+}
